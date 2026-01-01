12da6ff6480478c826ff79cb3eeb67f4a4212a8c
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-01-01 09:35:48 +0530

    1st january 2026 - 11 day streak

---
+++ 01012026_66_Plus One.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits){
+        int carry = 1;
+        int n = digits.size();
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]+carry >= 10){
+                digits[i] = (digits[i] + 1)%10;
+                carry = 1;
+            }
+            else{
+                digits[i] = digits[i] + carry;
+                carry = 0;
+            }
+        }        
+        if(carry == 1){
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
