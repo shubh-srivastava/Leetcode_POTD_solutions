af883869d53a9fd3ae42ad30a422e9456f923f71
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-01-24 10:25:47 +0530

    only way out is through

---
+++ 24012026_1877_Minimize Maximum Pair Sum in Array.cpp
@@ -0,0 +1,15 @@
+// 24TH jAN 2026
+
+class Solution {
+public:
+    int minPairSum(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+        int maxSum = 0;
+        int n = nums.size();
+
+        for(int i=0;i<n/2;i++){
+            maxSum = max(maxSum,nums[i] + nums[n-1-i]);
+        }
+        return maxSum;
+    }
+};
