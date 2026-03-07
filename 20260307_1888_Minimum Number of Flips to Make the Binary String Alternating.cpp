ab217c14cebfa2fed88099d7d37854748b417e4b
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-03-07 10:47:03 +0530

    7th march 2026

---
+++ 20260307_1888_Minimum Number of Flips to Make the Binary String Alternating.cpp
@@ -0,0 +1,21 @@
+// 7th march 2026
+
+class Solution {
+public:
+    int minFlips(string s){
+        int n = s.size();
+        int res = n;
+        vector<int>op(2,0);
+
+        for(int i=0;i<n;i++){
+            op[(s[i] ^ i) & 1]++;
+        }        
+
+        for(int i=0;i<n;i++){
+            op[(s[i] ^ i) & 1]--;
+            op[(s[i] ^ (n+i)) & 1]++;
+            res = min({res,op[0],op[1]});
+        }
+        return res;
+    }
+};
