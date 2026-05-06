401b7c8f3a219cfce2f005eb6a161ad52ea3be42
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-05-06 09:26:49 +0530

    6th may 2026

---
+++ 2026_05/20260506_1861_Rotating the Box.cpp
@@ -0,0 +1,30 @@
+// 6th May 2026
+
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid){
+        int n = boxGrid.size();
+        int m = boxGrid[0].size();
+
+        for(int i=0;i<n;i++){
+            int empty = m-1;
+            for(int j=m-1;j>=0;j--){
+                if(boxGrid[i][j] == '*'){
+                    empty = j - 1;
+                } 
+                else if(boxGrid[i][j] == '#'){
+                    swap(boxGrid[i][j], boxGrid[i][empty]);
+                    empty--;
+                }
+            }
+        }
+        vector<vector<char>> res(m, vector<char>(n));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                res[j][n - 1 - i] = boxGrid[i][j];
+            }
+        }
+
+        return res;      
+    }
+};
