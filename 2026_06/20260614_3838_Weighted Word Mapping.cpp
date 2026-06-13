01444f6c18825c9a3cd9f0f888e7943efedfbcf6
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-06-14 00:02:29 +0530

    14th june 2026

---
+++ 2026_06/20260614_3838_Weighted Word Mapping.cpp
@@ -0,0 +1,18 @@
+// 14th June 2026
+
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string res(words.size(), 0);
+        int i = 0;
+
+        for(auto& word : words){
+            int s = 0;
+            for(auto& c : word){
+                s += weights[(c & (1 << 5) - 1) - 1];
+            }
+            res[i++] = 'z' - (s - ((s * 2521) >> (1 << 4)) * 26);
+        }
+        return res;
+    }
+};
