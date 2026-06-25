b9d3c8e40ede6f1d0c10a1e223a0269ea3abcaf7
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-06-25 19:46:02 +0530

    25th june 2026

---
+++ 2026_06/20260625_3737_Count Subarrays With Majority Element I.cpp
@@ -0,0 +1,30 @@
+// 26th June 2026
+
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> pre(2*n+1,0);
+        pre[n] = 1;
+
+        int cnt = n;
+        long long presum = 0;
+        long long ans = 0;
+        for(int x : nums){
+            if(x == target){
+                presum += pre[cnt];
+                ++cnt;
+                ++pre[cnt];
+            }
+            else{
+                --cnt;
+                presum -= pre[cnt];
+                ++pre[cnt];
+            }
+            ans += presum;
+        }
+
+        return ans;
+    }
+};
