b84876a6d8a541c0d9f6ce3bb277d5d71cf8dab1
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-07-18 13:33:03 +0530

    18th july 2026

---
+++ 2026_07/20260718_1979_Find Greatest Common Divisor of Array.cpp
@@ -0,0 +1,10 @@
+// 18th July 2026
+
+class Solution {
+public:
+    int findGCD(vector<int>& nums){
+        int mx = *max_element(nums.begin(),nums.end());
+        int mn = *min_element(nums.begin(),nums.end());
+        return __gcd(mx,mn);        
+    }
+};
