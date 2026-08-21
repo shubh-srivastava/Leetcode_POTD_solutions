8b933fae08154cb99cd489a698954b4a353de256
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-08-21 23:33:54 +0530

    21st august 2026

--- 2026_08/20260820_3069_Distribute Elements Into Two Arrays I.cpp
+++ 2026_08/20260820_3069_Distribute Elements Into Two Arrays I.cpp
@@ -1,2 +1,14 @@
 // 20th August 2026
 
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums){
+        vector<int> A[2]={{nums[0]}, {nums[1]}};
+        const int n=nums.size();
+        for(int i=2; i<n; i++){
+            A[A[0].back()<=A[1].back()].push_back(nums[i]);
+        }
+        A[0].insert(A[0].end(), A[1].begin(), A[1].end());
+        return A[0];
+    }
+};
