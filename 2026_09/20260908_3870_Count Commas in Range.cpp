545c450fbb2c9a95a0269d4f11ee4ff5b70ac442
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-09-09 08:24:06 +0530

    9th september 2026

--- 2026_09/20260908_3870_Count Commas in Range.cpp
+++ 2026_09/20260908_3870_Count Commas in Range.cpp
@@ -1,2 +1,12 @@
 // 8th September 2026
 
+class Solution {
+public:
+    int countCommas(int n){
+        if(n < 1000){
+            return 0;
+        }
+        return (n - 1000) + 1;
+        
+    }
+};
