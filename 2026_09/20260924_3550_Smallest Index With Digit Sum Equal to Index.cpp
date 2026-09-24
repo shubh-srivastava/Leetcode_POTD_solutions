300dfd5e575fde2802e2af7a94157051b38d9ea9
shubh-srivastava <srivastavashubh2004@gmail.com>
2026-09-24 09:16:45 +0530

    24th september 2026

---
+++ 2026_09/20260924_3550_Smallest Index With Digit Sum Equal to Index.cpp
@@ -0,0 +1,21 @@
+// 24th September 2026
+
+class Solution {
+public:
+    int sumDigits(int num){
+        int sum = 0;
+        while(num > 0){
+            sum += num%10;
+            num = num/10;
+        }
+        return sum;
+    }
+    int smallestIndex(vector<int>& nums){
+        for(int i=0;i<nums.size();i++){
+            if(sumDigits(nums[i]) == i){
+                return i;
+            }
+        }    
+        return -1;    
+    }
+};
